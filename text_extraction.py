865a7f0bedc4e524c9891ea9b32dd92e21b25fbc
Jason Rodrigues <jasonrodrigues28@gmail.com>
2025-03-22 13:13:50 +0530

    Splitting the sentences into chunks are done

--- text_extraction.py
+++ text_extraction.py
@@ -10,4 +10,20 @@ def extract_text_from_pdf(pdf_path):
     return text
 
 pdf_text = extract_text_from_pdf("t.pdf")
-print(pdf_text)
+#print(pdf_text)
+
+#making senetces into chunks
+chunks = []
+current_chunk = ""
+
+sentences = pdf_text.split(". ")
+for i in sentences:
+    if len(current_chunk) + len(i) > 1000:
+        chunks.append(current_chunk)
+        current_chunk = ""
+    current_chunk += i + ". "
+
+if current_chunk:
+    chunks.append(current_chunk)
+
+print(chunks)
